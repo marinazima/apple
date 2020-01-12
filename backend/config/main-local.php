d14804c069e19c5f1b6e82bc06df81938daccc26
Zima <zzma@inbox.ru>
2020-01-13 01:01:23 +0300

    add locals

---
+++ backend/config/main-local.php
@@ -0,0 +1,28 @@
+<?php
+
+$config = [
+    'components' => [
+        'request' => [
+            // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
+            'cookieValidationKey' => 'ScqaOQIyM3zsw1o8oXrErNz6I0-3kIsK',
+        ],
+    ],
+];
+
+if (!YII_ENV_TEST) {
+    // configuration adjustments for 'dev' environment
+    $config['bootstrap'][] = 'debug';
+    $config['modules']['debug'] = [
+        'class' => 'yii\debug\Module',
+        'allowedIPs' => [
+            '*',
+        ],
+    ];
+
+    $config['bootstrap'][] = 'gii';
+    $config['modules']['gii'] = [
+        'class' => 'yii\gii\Module',
+    ];
+}
+
+return $config;
